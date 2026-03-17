cc775a18b8fc03c7cca1fbd80e297138943eb9c8
ВашÐOrest <_206orest122006@gmail.com>
2026-03-17 22:10:26 +0200

    Дод

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-import './style.css'
-// Читаємо параметр з адресного рядка (наприклад, ?task=4)
 const urlParams = new URLSearchParams(window.location.search);
 const task = urlParams.get('task');
 
@@ -12,9 +10,9 @@ if (task) {
     document.getElementById('backBtn').style.display = 'block';
 
     // 3. Динамічно завантажуємо потрібний файл завдання
-    if (task === '1') import('./task1.js');
-    if (task === '2') import('./task2.js');
-    if (task === '3') import('./task3.js');
-    if (task === '4') import('./task4.js');
+    if (task === '1') import('./src/task1.js');
+    if (task === '2') import('./src/task2.js');
+    if (task === '3') import('./src/task3.js');
+    if (task === '4') import('./src/task4.js');
 }
 // Якщо параметра немає (ми щойно зайшли на сайт), нічого не робимо – показується меню.і
